043c6d140eaa61cc7c499fb41fb951a25ef4f6ea
MarvinTaterra <115582964+MarvinTaterra@users.noreply.github.com>
2024-08-07 16:29:27 +0200

    Update vis_pymol.py

--- mdpath/vis_pymol.py
+++ mdpath/vis_pymol.py
@@ -11,6 +11,11 @@ def visualize_pdb(pdb_path):
     cmd.show("cartoon", "polymer.protein")
     cmd.show("sticks", "organic")
     cmd.hide("everything", "resn POP or hydrogens or inorganic")
+    cmd.set_color('mdp_gray', [0.498, 0.498, 0.498])
+    cmd.bg_color('white')
+    cmd.select('polymer_protein', 'polymer and protein')
+    cmd.color('mdp_gray', 'polymer_protein')
+    
 
 
 def visualize_clusters(json_path):
@@ -20,7 +25,7 @@ def visualize_clusters(json_path):
     for prop in clusters:
         pos0 = [prop["coord1"][0], prop["coord1"][1], prop["coord1"][2]]
         pos1 = [prop["coord2"][0], prop["coord2"][1], prop["coord2"][2]]
-        r = prop["radius"] * 10
+        r = prop["radius"]
         color = [prop["color"][0], prop["color"][1], prop["color"][2]]
         cylinder = [cgo.CYLINDER, *pos0, *pos1, r, *color, *color]
         cmd.load_cgo(cylinder, f"path_{count}")
@@ -29,8 +34,12 @@ def visualize_clusters(json_path):
 
 # The function to be called by PyMOL with provided arguments
 def main(pdb_path, json_path):
+    print(f"pdb_path: {pdb_path}")
+    print(f"json_path: {json_path}")
     visualize_pdb(pdb_path)
     visualize_clusters(json_path)
+    cmd.center('polymer_protein')
+    
 
 
 cmd.extend("mdpath", main)
